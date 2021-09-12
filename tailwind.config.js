6dfba492c093354cec5c2533465dd73c95825df0
Abderrahmen Brinis <72577453+dahmenbriniss@users.noreply.github.com>
2021-09-12 22:24:12 +0100

    added the contact me section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     mode: 'jit',
+
     purge: {
         content: ["public/**/*.html"],
     },
     darkMode: true,
     theme: {
         fontFamily: {
-            body: ['Open Sans', "sans-serif"]
+            body: ['Poppins', 'Open Sans', "sans-serif"]
         },
     },
     variants: {
